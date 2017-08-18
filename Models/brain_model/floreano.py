55d2ff53664b1a58f3105f447bcdfa160ff1f39d
Stefan Walke <stefan.walke@tum.de>
2017-08-18 14:44:12 +0200

    started integration of genetic information into brain construction

--- Models/brain_model/floreano.py
+++ Models/brain_model/floreano.py
@@ -12,11 +12,21 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+l = np.zeros([10,33]) 
+"""
+Structure of the dna array: 
+-Binary values
+-Each row repersents a brain neuron
+-First bit: the neurons axons are connected to inhibitory (0) or excitatory (1) synapses
+-Next 18 bits: which sensory neurons are connected (1) or disconnected (0) to/from the neuron (only exitatory synapses)
+-Last 10 bits: which brain neurons is the current neuron connected (1) to, self connectionbs allowed
+"""
 
-def create_brain():
+def create_brain(dna = l):
     """
     Initializes PyNN with the neuronal network that has to be simulated
     """
+
     SENSORPARAMS = {'v_rest': -60.5,
                     'cm': 0.025,
                     'tau_m': 4.0,
@@ -38,17 +48,6 @@ def create_brain():
                     'e_rev_I': -75.0,
                     'v_thresh': -60.4,
                     'v_reset': -60.5}
-    
-    ACTORPARAMS = {'v_rest': -60.5,
-                    'cm': 0.025,
-                    'tau_m': 4.0,
-                    'tau_refrac': 2.0,
-                    'tau_syn_E': 10.0,
-                    'tau_syn_I': 10.0,
-                    'e_rev_E': 0.0,
-                    'e_rev_I': -75.0,
-                    'v_thresh': -60.4,
-                    'v_reset': -60.5}
 
     SYNAPSE_PARAMS = {"weight": 1,
                       "delay": 2.0,
@@ -56,10 +55,9 @@ def create_brain():
                       'tau_rec': 1.0,
                       'tau_facil': 1.0}
 
-    population = sim.Population(32, sim.IF_cond_alpha())
+    population = sim.Population(28, sim.IF_cond_alpha())
     population[0:18].set(**SENSORPARAMS)
     population[18:28].set(**BRAINPARAMS)
-    population[28:32].set(**ACTORPARAMS)
 
     # Connect neurons
     CIRCUIT = population
@@ -78,11 +76,6 @@ def create_brain():
                    synapse_type=SYN,
                    receptor_type='excitatory')
 
-    sim.Projection(presynaptic_population=CIRCUIT[18:28],
-                   postsynaptic_population=CIRCUIT[28:32],
-                   connector=sim.AllToAllConnector(allow_self_connections=False),
-                   synapse_type=SYN,
-                   receptor_type='excitatory')
 
     sim.initialize(population, v=population.get('v_rest'))
 
