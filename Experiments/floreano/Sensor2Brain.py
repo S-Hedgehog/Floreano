changed rate calculation and fixed bugs cleaned up brain

Experiments/floreano/Sensor2Brain.py
@@ -4,26 +4,38 @@ dna = np.random.randint(2,size=(10,29))
 @nrp.MapRobotSubscriber("camera", Topic('/husky/camera', sensor_msgs.msg.Image))  
 @nrp.MapVariable("ideal_wheel_speed", global_key="ideal_wheel_speed", initial_value=[0.0,0.0], scope=nrp.GLOBAL)
 @nrp.MapVariable("real_wheel_speed", global_key="real_wheel_speed", initial_value=[0.0,0.0], scope=nrp.GLOBAL)
-@nrp.MapSpikeSource("neurons", nrp.map_neurons(range(10), lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r1", nrp.map_neurons(nrp.brain.receptors[0], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r2", nrp.map_neurons(nrp.brain.receptors[1], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r3", nrp.map_neurons(nrp.brain.receptors[2], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r4", nrp.map_neurons(nrp.brain.receptors[3], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r5", nrp.map_neurons(nrp.brain.receptors[4], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r6", nrp.map_neurons(nrp.brain.receptors[5], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r7", nrp.map_neurons(nrp.brain.receptors[6], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r8", nrp.map_neurons(nrp.brain.receptors[7], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r9", nrp.map_neurons(nrp.brain.receptors[8], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r10", nrp.map_neurons(nrp.brain.receptors[9], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r11", nrp.map_neurons(nrp.brain.receptors[10], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r12", nrp.map_neurons(nrp.brain.receptors[11], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r13", nrp.map_neurons(nrp.brain.receptors[12], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r14", nrp.map_neurons(nrp.brain.receptors[13], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r15", nrp.map_neurons(nrp.brain.receptors[14], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r16", nrp.map_neurons(nrp.brain.receptors[15], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r17", nrp.map_neurons(nrp.brain.receptors[16], lambda i: nrp.brain.brain[i]), nrp.poisson)
+@nrp.MapSpikeSource("r18", nrp.map_neurons(nrp.brain.receptors[17], lambda i: nrp.brain.brain[i]), nrp.poisson)
 @nrp.Robot2Neuron()
-def Sensor2Brain(t, ideal_wheel_speed, real_wheel_speed, camera, neurons):
+def Sensor2Brain(t, ideal_wheel_speed, real_wheel_speed, camera, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18):
     bridge = CvBridge()
-    dna = np.random.randint(2,size=(10,29))
     if not isinstance(camera.value, type(None)):
         (thresh, im_bw) = cv2.threshold(bridge.imgmsg_to_cv2(camera.value, "mono8"), 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        iws = ideal_wheel_speed.value
-        rws = real_wheel_speed.value
-        r = np.zeros((10))
-        row_counter=0
-        for row in dna:
-            for n in range(18):
-                if row[n+1] == 1:
-                    if n == 16:
-                        r[row_counter] = r[row_counter] + np.absolute(100.0*iws[0]-rws[0])
-                    elif n == 17:
-                        r[row_counter] = r[row_counter] + np.absolute(100.0*iws[1]-rws[1])
-                    else:
-                        r[row_counter] = r[row_counter] + (im_bw.item(n)/6.0) + (im_bw.item(n+1)/3.0) + (im_bw.item(n+2)/3.0) + (im_bw.item(n+3)/6.0)
-            row_counter+=1
-        for x in range(10):
-            neurons[x].rate = r[x]
+        iws = np.array(ideal_wheel_speed.value)
+        rws = np.array(real_wheel_speed.value)
+        visual_receptors = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16]
+        for i in range(len(visual_receptors)):
+            visual_receptors[i].rate = im_bw.item((4*i))/6.0 + im_bw.item((4*i)+1)/3.0 + im_bw.item((4*i)+2)/3.0 + im_bw.item((4*i)+3)/6.0
+            clientLogger.info(visual_receptors[i].rate)
+        for n in range(len(r17)):
+            r17[n].rate = 100000*np.absolute(iws[0]-rws[0])
+        for n in range(len(r18)):
+            r18[n].rate = 100000*np.absolute(iws[1]-rws[1])
+        clientLogger.info(r17.rate)
+        clientLogger.info(r18.rate)

Models/brain_model/floreano.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 """
 This file contains the setup of the neuronal network running the Floreano experiment with neuronal image recognition
 """
@@ -12,7 +12,11 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-l = np.random.randint(2,size=(10,29))
+dna = np.random.randint(2,size=(10,29))
+receptors = []
+for r in range(1,19):
+    receptors.append(np.nonzero(dna[:,r])[0])
+
 """
 Structure of the dna array: 
 -Binary values
@@ -22,20 +26,19 @@ Structure of the dna array:
 -Last 10 bits: which brain neurons is the current neuron connected (1) to, self connectionbs allowed
 """
 
-def create_brain(dna = l):
+def create_brain():
     """
     Initializes PyNN with the neuronal network that has to be simulated
     """
 
     NEURONPARAMS = {'v_rest': -60.5,
-                    'cm': 0.525,
                     'tau_m': 4.0,
                     'tau_refrac': 2.0,
                     'tau_syn_E': 10.0,
                     'tau_syn_I': 10.0,
                     'e_rev_E': 0.0,
                     'e_rev_I': -75.0,
-                    'v_thresh': -57.0,
+                    'v_thresh': -60.4,
                     'v_reset': -60.5}
 
     SYNAPSE_PARAMS = {"weight": 1.0,