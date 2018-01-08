760109592f2b5c11214fb517b00e829bb98bd42c
Stefan Walke <stefan.walke@tum.de>
2018-01-08 15:23:52 +0100

    Fixed Bugs with the script

---
+++ insert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+source $HOME/.bashrc
+
+if [ -d "$HBP" ] 
+then
+    echo "Directory $HBP exists." 
+else
+    echo "Error: Directory $HBP does not exists."
+fi
+
+echo
+echo =============================
+echo Deploying Floreano Experiment
+echo =============================
+
+cp -H - -u Models/brain_model/floreano.py $HBP/Models/brain_model
+
+cp -H -v -r -u Models/floreano_box $HBP/Models
+
+cp -H -v -r -u Experiments/floreano $HBP/Experiments
+
+sed -i -e '/floreano/ d' $HBP/Experiments/_rpmbuild/folders.txt
+echo "floreano" >> $HBP/Experiments/_rpmbuild/folders.txt
+
+cd $HBP/Models
+./create-symlinks.sh
+cd $HBP/gzweb
+./deploy-gzbridge-nrp.sh
+cd $HBP/user-scripts
+./configure_nrp
